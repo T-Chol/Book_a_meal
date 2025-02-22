e19ca04e5ccbc70848e314ad08a16841c00f7538
Reddie <reddiegtech@gmail.com>
2025-02-22 14:46:41 +0300

    Head

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,8 +1,10 @@
 // client/src/app/page.tsx
 import Image from "next/image";
+import Header from '../components/header.js';
 
 export default function Home() {
-return (
+return (<>
+<Header/>
 <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
     <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
     <Image
@@ -98,5 +100,6 @@ return (
     </a>
     </footer>
 </div>
+</>
 );
 }
