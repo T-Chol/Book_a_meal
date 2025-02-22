4a7db2099a800f17fe2a4b96b6135363d4552566
Reddie <reddiegtech@gmail.com>
2025-02-22 22:07:53 +0300

    optimizes pictures

--- client/next.config.ts
+++ client/next.config.ts
@@ -8,6 +8,22 @@ eslint: {
 typescript: {
     ignoreBuildErrors: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'via.placeholder.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.themealdb.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
