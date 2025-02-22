b3dd99f4ec8475feef8d467559491867b36dda91
Reddie <reddiegtech@gmail.com>
2025-02-22 13:45:18 +0300

    relationships

--- server/models/myCart.py
+++ server/models/myCart.py
@@ -11,7 +11,9 @@ class MyCart(db.Model):
     quantity = db.Column(db.Integer, nullable=False, default=1)
 
     # Relationships
-    user = db.relationship("User", backref="cart_items")
+    # In MyCart
+    user = db.relationship("User", backref="cart_items", overlaps="carts")
+    # user = db.relationship("User", backref="cart_items")
     meal = db.relationship("Menu", backref="cart_orders")
     __table_args__ = {"extend_existing": True}
 
